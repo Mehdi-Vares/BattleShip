57bf815e7f291ac776226737c67c66520aef0c6d
Mehdi-Vares <mr.mehdi.vares@gmail.com>
2023-01-17 02:53:44 +0330

    commit: some bugs were fixed.

--- BattleShip/src/Message.java
+++ BattleShip/src/Message.java
@@ -2,7 +2,6 @@ import java.io.Serializable;
 
 public class Message implements Serializable {
     private int x, y;
-    private boolean isOpp;
     private boolean isMissed;
 
     public Message(int x, int y, boolean isMissed) {
